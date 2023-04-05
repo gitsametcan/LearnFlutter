4efce46c93651286c6ce656213466a6482ac2720
Samet CAN <108118230+gitsametcan@users.noreply.github.com>
2023-04-06 01:08:30 +0300

    Continue flutter text

--- flutter_projects/flutter_full_learn/lib/baslangic/text_learn.dart
+++ flutter_projects/flutter_full_learn/lib/baslangic/text_learn.dart
@@ -2,18 +2,36 @@ import 'package:flutter/material.dart';
 
 class TextLearnView extends StatelessWidget {
   const TextLearnView({Key? key}):super(key: key);
+  final String name = 'samet';
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      body: Center(child: Text(
-        ('sam' * 10),
+      body: Center(
+        child:
+        Text(
+        ('Ben $name ${name.length}'),
         maxLines: 2,
         overflow: TextOverflow.ellipsis,
+        textAlign: TextAlign.right,
+        textScaleFactor: 1,
         
-        style: const TextStyle(
-          fontWeight: FontWeight.w600,wordSpacing: 2, letterSpacing: 2,fontSize: 16,
+        //style: ProjectStyles.welcomeStyle,
+        style: Theme.of(context).textTheme.headline5?.copyWith(
+          color: Colors.red,
         ),
-        )),
+        )
+        
+        ),
     );
   }
+}
+
+class ProjectStyles {
+  static TextStyle welcomeStyle = const TextStyle(
+    fontWeight: FontWeight.w800,
+    wordSpacing: 5, 
+    letterSpacing: 2,
+    fontSize: 16,
+    color: Colors.lime
+  );
 }
